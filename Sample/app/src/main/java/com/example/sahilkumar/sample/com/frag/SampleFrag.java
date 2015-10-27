de0e4f253d78d0f5422bdf47a95dbe9acc2481e3
SAHIL KUMAR <SAHIL KUMAR>
2015-10-27 15:54:03 +0530

    fix the bug

---
+++ Sample/app/src/main/java/com/example/sahilkumar/sample/com/frag/SampleFrag.java
@@ -0,0 +1,26 @@
+package com.example.sahilkumar.sample.com.frag;
+
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sahilkumar.sample.R;
+
+/**
+ * Created by SAHIL KUMAR on 27-10-2015.
+ */
+public class SampleFrag extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_main,container);
+
+        return rootView;
+
+
+    }
+}
